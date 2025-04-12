9bed3b91aea3fe5b67634a2aaf67d83f660fcdd2
Dan Ki <danki92@gmail.com>
2025-04-12 10:18:19 -0400

    modularized setup

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -3,9 +3,18 @@
 -- config. This will add also the recommended keymaps.
 
 return {
+  -- See `:help gitsigns` to understand what the configuration keys do
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
   {
     'lewis6991/gitsigns.nvim',
     opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
